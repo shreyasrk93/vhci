ef96c6d9337ec836e19594a8556efdb0bc2c1d36
rohithpr <praroh2@gmail.com>
2016-01-09 16:53:01 +0530

    Add operations to demo

--- demo_app/static/js/home.js
+++ demo_app/static/js/home.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
+  (function() {
+    // Initial states have to be defined here
+    // $(".device_state").css("display", "block")
+    // $(".available_operations").css("display", "none")
+    $(".device_state").css("display", "none")
+    $(".available_operations").css("display", "block")  })()
+
+  $("#show_states").click(function(e) {
+    e.preventDefault()
+    $(".device_state").css("display", "block")
+    $(".available_operations").css("display", "none")
+  })
+
+  $("#show_commands").click(function(e) {
+    e.preventDefault()
+    $(".device_state").css("display", "none")
+    $(".available_operations").css("display", "block")
+  })
+
   $("#command_form").submit(function(e) {
     e.preventDefault()
     var command = $("input[name=command_text]").val()
     $.ajax({
-      url: '/command',
-      method: 'POST',
+      url: "/command",
+      method: "POST",
       data: {
         command: command
       },
